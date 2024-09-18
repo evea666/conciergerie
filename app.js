4ff2c4d2b73247aff24a142f992b9e354b031ceb
evea666 <135027289+evea666@users.noreply.github.com>
2024-09-18 12:16:52 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.liste-nav');
+const btnmenu = document.querySelector('.menu');
+
+
+btnmenu.addEventListener('click',function(){
+    menu.classList.toggle('active-menu')
+})
